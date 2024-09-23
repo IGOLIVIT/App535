7a4a9d8c0a387dc4a9d8c95d81edf257f13761f9
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-09-23 12:51:12 +0300

    Initial Commit

---
+++ App535/App535App.swift
@@ -0,0 +1,17 @@
+//
+//  App535App.swift
+//  App535
+//
+//  Created by IGOR on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct App535App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
